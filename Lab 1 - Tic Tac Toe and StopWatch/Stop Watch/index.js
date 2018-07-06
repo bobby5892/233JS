bc4da28d12774513999a32ad69f2b8009e78b3dc
Robert Moore <robert.ben.moore@gmail.com>
2018-07-06 11:43:41 -0700

    updated

--- Lab 1 - Tic Tac Toe and StopWatch/Stop Watch/index.js
+++ Lab 1 - Tic Tac Toe and StopWatch/Stop Watch/index.js
@@ -12,12 +12,12 @@ class StopWatch {
         -   All of the functionality of init will happen in the constructor.
         -   Add the event handlers for the start, stop and reset buttons.
             You'll have to bind(this) to each function because the keyword
-            this will refer to the button (not the class) when the 
+            this will refer to the button (not the class) when the
             event fires
             -- this.startButton.onclick = this.startTimer.bind(this);
     */
     constructor() {
-        
+
          /*
         Create 3 global variables, isRunning, timer and timerTime.
         Initialize them to false, null and 0 respectively.
@@ -34,19 +34,24 @@ class StopWatch {
         this.timerSeconds = document.getElementById("seconds");
 
         this.init();
-        
+
     }
 
     init() {
         // Put the element on the page with an id of start in a variable
         // Do the same for the stop button and the reset button
-       
+
 
         // Add an onclick handler to each of the buttons
-        // Use the functions startTimer, stopTimer and resetTimer 
+        // Use the functions startTimer, stopTimer and resetTimer
         this.buttonStart.addEventListener("click", () => { this.startTimer(); });
         this.buttonStop.addEventListener("click", () => { this.stopTimer(); });
         this.buttonReset.addEventListener("click", () => { this.resetTimer(); });
+
+        // Some CSS
+        this.buttonStart.classList.add("btn-primary");
+        this.buttonStop.classList.add("btn-secondary");
+        this.buttonReset.classList.add("btn-danger");
     }
 
 
@@ -73,7 +78,7 @@ class StopWatch {
             // write the minutes and seconds to the elements on the page
             this.timerMinutes.innerHTML = this.pad(minutes);
             this.timerSeconds.innerHTML = this.pad(seconds);
-            
+
             // use the function pad to make sure there's a leading 0 if necessary
         }
     }
@@ -91,6 +96,7 @@ class StopWatch {
         // if the timer is running, stop it
         if (this.isRunning) {
             this.isRunning = false;
+            clearInterval(this.timer);
             this.timer = null;
         }
 
@@ -101,7 +107,7 @@ class StopWatch {
         // stop the timer
         this.isRunning = false;
         clearInterval(this.timer);
-        
+
         // set the timerTime back to 0
         this.timerTime = 0;
         // write 00 to the elements on the page for minutes and seconds
@@ -116,7 +122,7 @@ class StopWatch {
 
 // Create a variable called stopWatch
 let stopWatch;
-// Add an event handler to the load event of the window. 
+// Add an event handler to the load event of the window.
 window.addEventListener("load", () => {
     stopWatch = new StopWatch();
 });
